849e777a66a26b4f3b4162bc8409fbacbad9d589
Napoléon Bonaparte <ximliu0228aicmz@gmail.com>
2021-02-22 11:21:26 +0800

    Update ocserv_radius_quickinstall.sh

--- ocserv_radius_quickinstall.sh
+++ ocserv_radius_quickinstall.sh
@@ -1,6 +1,6 @@
 sudo -i
 yum install wget -y 
-wget https://raw.githubusercontent.com/chendong12/ocserv/master/radius_for_ocserv.sh
+wget https://raw.githubusercontent.com/ximliu/ocserv/master/radius_for_ocserv.sh
 chmod +x radius_for_ocserv.sh
 ./radius_for_ocserv.sh
 #
